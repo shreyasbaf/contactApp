8ca1d61aa8a3b663e60df539439dfbe151a3eb99
shreyasbaf <shreyas7bafna@gmail.com>
2021-11-09 12:54:15 +0530

    Initial Commit

---
+++ src/Components/ItemCard.js
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import { Button, CardActions } from '@mui/material';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import { FaEnvelope, FaGlobe, FaHeart, FaRegHeart, FaTrashAlt, FaUserAlt } from "react-icons/fa";
+
+export default function ItemCard(props) {
+    console.log(props.data)
+  return (
+      <div>
+    <Card>
+    <CardMedia
+        component="img"
+        height="194"
+        image={`https://avatars.dicebear.com/v2/avataaars/${props.data.name}.svg?options[mood][]=happy`}
+        alt={props.data.name}
+      />
+      <CardContent>
+        <Typography variant="body2" color="text.secondary">
+      <FaUserAlt /> {props.data.name}<br/>
+      <FaEnvelope /> {props.data.email}<br />
+      <FaGlobe /> {props.data.website}
+      </Typography>
+      </CardContent>
+      <CardActions >
+      <Button style={{color:'red'}} onClick={() => props.onHandleLike(props.data.id)}>{props.data.like ? 
+      <FaHeart/>  : <FaRegHeart /> }
+      </Button>
+      <Button onClick={() => props.onHandleClick(props.data.id)}> <FaTrashAlt /> </Button>
+      
+      </CardActions>
+    </Card>
+    </div>
+  );
+}
+
---
+++ src/Components/Main.js
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import axios from 'axios';
+import ItemCard from './ItemCard';
+import CircularProgress from '@mui/material/CircularProgress';
+
+export default function Main(props) {
+    const [loading,setLoading] = React.useState(false)
+    const [user,setUser] = React.useState([])
+
+    React.useEffect(() => {
+        setLoading(true)
+        axios.get(`https://jsonplaceholder.typicode.com/users`)
+        .then(response =>{
+            var resData = response.data
+            resData = resData.map(item =>{
+              item.like = false
+              return item
+            })
+            setUser(resData)
+            setLoading(false)
+        })
+        .catch(error =>{
+            console.log('ERROR',error)
+        })
+    },[])
+
+function onHandleClick(value){
+    const newArray = user.filter(ele => 
+        ele.id !== value)
+    setUser(newArray)
+}
+
+function onHandleLike(value){
+    const newArray = user.map(ele =>{
+        if(ele.id === value){
+            ele.like = !ele.like
+        }
+        return ele
+    })
+    setUser(newArray);
+}
+
+  return (
+    <div>
+        <h2 style={{justifyContent:'center',justifyItems:'center',textAlign:'center'}}>Contact App</h2>
+        {
+            loading ? 
+            <CircularProgress style={{justifyContent:'center', alignItems:'center',justifyItems:'center',textAlign:'center'}}/>
+            :    
+            <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+              {user.map((item, index) => (
+                <Grid item xs={12} sm={4} md={4} key={index} >
+                      <ItemCard data={item} onHandleClick={onHandleClick} onHandleLike = {onHandleLike}/>              
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+        }
+    </div>
+  );
+}
